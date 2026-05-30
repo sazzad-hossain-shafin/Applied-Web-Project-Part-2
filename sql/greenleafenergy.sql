8e274813702e1a8382572afb003eeaf9807fe1dd
sazzad-hossain-shafin <105111884@student.swin.edu.au>
2026-05-30 22:26:57 +1000

    Final submission

--- sql/greenleafenergy.sql
+++ sql/greenleafenergy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 30, 2026 at 02:00 PM
+-- Generation Time: May 30, 2026 at 02:25 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
